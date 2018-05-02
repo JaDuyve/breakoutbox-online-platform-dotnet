526bc45b74aaaa92c463141598fecad8b90454a8
Axel Dion <axel.dion.y9116@student.hogent.be>
2018-05-02 16:53:10 +0200

    test GroepControllerTest aanmaken

---
+++ breakoutbox.Tests/Controllers/GroepControllerTest.cs
@@ -0,0 +1,39 @@
+﻿using breakoutbox.Controllers;
+using breakoutbox.Models.Domain;
+using breakoutbox.Tests.Data;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace breakoutbox.Tests.Controllers
+{
+    public class GroepControllerTest
+    {
+        private readonly Mock<IGroepRepository> _mockGroepRepository;
+        private readonly Mock<ISessieRepository> _mockSessieRepository;
+        private readonly GroepController _groepController;
+        private readonly DummyApplicationDbContext _dummyContext = new DummyApplicationDbContext();
+
+        public GroepControllerTest()
+        {
+            _mockGroepRepository = new Mock<IGroepRepository>();
+            _mockSessieRepository = new Mock<ISessieRepository>();
+            _groepController = new GroepController(_mockGroepRepository.Object, _mockSessieRepository.Object);
+
+        }
+        
+        [Theory]
+        [InlineData(5)]
+        public void Start_Correcte_Pad_Retrourt_True(int id)
+        {
+            var result = _groepController.Start(id) as ViewResult;
+        }
+        
+        [Theory]
+        [InlineData(10)]
+        public void Start_Fout_Pad_Retrourt_False(int id)
+        {
+            var result = _groepController.Start(id) as ViewResult;  
+        }
+    }
+}
